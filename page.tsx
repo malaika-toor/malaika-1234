aa4b5dc15afaee422970a1bbc1b62994ca26e97b
malaika toor <164207619+malaika-toor@users.noreply.github.com>
2024-10-05 20:46:22 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Mcoder from './components/malaika/page'
+import Navbar from './components/navbar/page'
+
+const page = () => {
+  return (
+    <div>
+    <Navbar/>
+      <Mcoder/>
+
+    </div>
+  )
+}
+
+export default page
